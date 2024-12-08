b38b9063bf9f6398bb91f8cbb88398f3f9e1087c
CharlesBeck2001 <58281703+CharlesBeck2001@users.noreply.github.com>
2024-12-07 22:49:16 -0700

    Update Uniswap_Dash.py

--- Uniswap_Dash.py
+++ Uniswap_Dash.py
@@ -38,8 +38,7 @@ query = """
 SELECT *
 FROM `tristerotrading.uniswap.v3_trades`
 WHERE buy IN ('USDC', 'USDT')
-   OR sell IN ('USDC', 'USDT')
-LIMIT 100000;
+   OR sell IN ('USDC', 'USDT');
 
 """
 
